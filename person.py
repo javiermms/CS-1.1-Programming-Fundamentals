9d9d5861ce4b116f7a961a1b4682da98d35074a2
Javier Mendoza <javier.mendoza@students.makeschool.com>
2018-12-03 11:47:24 -0800

    worked on virus.py and person.py

--- person.py
+++ person.py
@@ -1,5 +1,6 @@
 import random
-# TODO: Import the virus clase
+# TODO: Import the virus class
+import virus 
 
 class Person(object):
     '''
@@ -44,14 +45,20 @@ class Person(object):
     def __init__(self, _id, is_vaccinated, infected=None):
         # TODO:  Finish this method.  Follow the instructions in the class documentation
         # to set the corret values for the following attributes.
-        self._id = None
-        self.is_vaccinated = None
-        self.is_alive = None
-        self.infected = None
+        self._id = _id
+        self.is_vaccinated = is_vaccinated
+        self.is_alive = True
+        self.infection = infected
 
 
-    def did_survive_infection():
+    def did_survive_infection(self):
         # TODO:  Finish this method. Follow the instructions in the class documentation
         # for resolve_infection.  If person dies, set is_alive to False and return False.
         # If person lives, set is_vaccinated = True, infected = None, return True.  
-        pass
+        if self.infection != None:
+            random_number = random.randint(0,1)
+            if random_number < self.infection.mortality_rate:
+                is_alive = False
+            elif random_number > self.infection.mortality_rate:
+                is_vaccinated = True
+                self.infected = None
